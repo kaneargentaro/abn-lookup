f69a39ef876e8966eefd88d99f8a31dc891a2ad6
Kane Argentaro <kane_argentaro@hotmail.com>
2025-11-19 19:32:14 +1100

    feat(web): use .env.local as default

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,13 @@
+import { resolve } from 'path';
 import type { NextConfig } from 'next';
+import { config } from 'dotenv';
+
+config({ path: resolve(__dirname, '../../.env.local') });
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
+    reactStrictMode: true,
+    poweredByHeader: false,
+    compress: true,
 };
 
 export default nextConfig;
